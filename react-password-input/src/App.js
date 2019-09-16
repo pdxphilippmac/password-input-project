8f0f396d48d5494f507c6c0ab9b52ea30dd10c4e
Philipp Maecke <p.maecke@gmx.de>
2019-09-16 14:56:27 +0200

    add colorChange via input

--- react-password-input/src/App.js
+++ react-password-input/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import PasswordInput from "./components/PasswordInput";
+// import AddPassword from "./components/PasswordInput";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello This is App</h1>
+      <PasswordInput />
     </div>
   );
 }
--- react-password-input/src/components/PasswordInput.js
+++ react-password-input/src/components/PasswordInput.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+function getInputStyle(passwordLength) {
+  let hue = 0 + passwordLength * 11;
+  if (hue < 124) {
+    return {
+      backgroundColor: `hsl(${hue}, 100%, 50%)`
+    };
+  } else if ((passwordLength = 10)) {
+    let hue = "124";
+    return { backgroundColor: `hsl(${hue}, 100%, 62%)` };
+  }
+
+  const inputStyle = {
+    backgroundColor: "hsl(2, 100%, 10%)"
+  };
+
+  return inputStyle;
+}
 
 function PasswordInput() {
-  return <input className="password-input" type="password" />;
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <input
+        style={getInputStyle(password.length)}
+        className="password-input"
+        type="password"
+        maxLength="20"
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+      />
+      <p>Password Length:{password.length}</p>
+      <p>Password: {password} </p>
+    </div>
+  );
 }
 
 export default PasswordInput;
