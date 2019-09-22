5fd02dcf5912f1547032371dceb06e26d612aa99
Philipp Maecke <p.maecke@gmx.de>
2019-09-22 23:43:14 +0200

    add input

--- react-password-input/src/components/ModalTextinput.js
+++ react-password-input/src/components/ModalTextinput.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const TextInput = styled.input`
-  border: #547fb3 solid 2px;
-  border-radius: 5px;
+  border: #547fb3 solid 2.5px;
+  border-radius: 12px;
   color: #707070;
   text-align: center;
   font-family: "Roboto", sans-serif;
   font-weight: bold;
-  opacity: 0.5;
+  font-size: 16px;
+  /* opacity: 0.5; */
   padding: 10px;
+  width: 304px;
+  border-color: #547fb3;
+  /* color: ${props => (props.primary ? "#707070" : "#547fb3")}; */
+  
 `;
 
-export default function DefaultInput() {
-  const [password, setPassword] = React.useState("");
-  return <TextInput value={password} onChange={setPassword} />;
-}
-
-export const InitialValueInput = () => {
-  const [password, setPassword] = React.useState(
-    "Bitte Restaurantnamen eingeben"
+export default function RestaurantInput(onChange, value) {
+  const [input, setInput] = useState("");
+  let pStyle = {
+    color: "#547fb3"
+  };
+  return (
+    <div>
+      {" "}
+      <p style={pStyle}>Name</p>
+      <TextInput
+        className="password-input"
+        type="text"
+        value={input}
+        onChange={event => {
+          setInput(event.target.value);
+        }}
+      />
+      <p> Input value: {input} </p>
+    </div>
   );
-  return <TextInput value={password} onChange={setPassword} />;
-};
+}
--- react-password-input/src/stories/ModalTextinput.stories.js
+++ react-password-input/src/stories/ModalTextinput.stories.js
@@ -1,9 +1,18 @@
 import React from "react";
-import Input from "../components/ModalTextinput";
+import RestaurantInput from "../components/ModalTextinput";
 
 export default {
   title: "ModalInput"
 };
 
-export const DefaultInput = () => <Input />;
-export const InitialValueInput = () => <Input />;
+export const withoutProps = () => <RestaurantInput />;
+
+export const Default = () => {
+  const [password, setPassword] = React.useState("");
+  return <RestaurantInput value={password} onChange={setPassword} />;
+};
+
+export const WithInitialValue = () => {
+  const [password, setPassword] = React.useState("password123");
+  return <RestaurantInput value={password} onChange={setPassword} />;
+};
